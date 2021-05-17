8e4ba17428692766e16a3ae1ecea0b074520ae59
Mervens Vic <82620500+Mervens@users.noreply.github.com>
2021-05-17 01:51:45 -0400

    Fixing

--- Password Generator C3/Develop/script.js
+++ Password Generator C3/Develop/script.js
@@ -25,43 +25,70 @@ window.alert("Welcome to the Password Generator Challenge Assignment!");
 function userPrompts() {
 alert("Let's begin!");
 alert("Acceptable units for this generator are letters, numbers, and/or symbols.");
-alert("Let's set the length of your password.");
-var passL = prompt("What length do you want? 8-128 characters.");
+alert("Click Generate Password to begin.");
+}
+
+
+const generate = () => {
+    let passL = prompt("What length do you want? 8-128 characters.");
+    let passS = "";
+
 if (passL > 8 && passL < 128) {
     characterSet.forEach(set => {
-    const insChar = (prompt('Do you want to use ${set.name}?')).toLowerCase();
+    const insChar = (prompt(`Do you want to use ${set.name}?`)).toLowerCase();
     if (insChar === "yes" || insChar === "y") {
         set.use = true;
+    }else{
+        alert("Then we will not use this item.");
+        return
     }
     if(set.use){
         randChar = randChar + set.char;
+    }else{
+        alert("You have not entered any conditions. This is invalid.");
+        return
     }
-    });
- 
-}else{
-alert("Your password does not fit between 8-128. Try again.");
-return
-}
-};
+    }
+    );
+    }else{
+        alert("Your password does not fit between 8-128. Try again.");
+        return
+    }
 
-  
+if (randChar !== "") {
+    for (i = 1; i <= passL; i++) {
+        password = passS + randChar.charAt(Math.floor(Math.random() * Math.floor((randChar.length) - 1)));
+    }
+    document.getElementById("password").value = passS;
+}
+else {
+    alert("There are no characters inputted. Password cannot be generated.");
+}
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-document.getElementById('#generate').onclick =  userPrompts();
+
 
 // Write password to the #password input
 function writePassword() {
-  userI = "";
-  finalP = "";
-  display = "";
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    userI = "";
+    finalP = "";
+    display = "";
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    bclick = document.getElementById('generate');
+    bclick.onclick = clicked;
 
   passwordText.value = password;
 
 }
 
+function clicked() {
+     const div = document.createElement('div');
+     div.textContent = 'Generated';
+     document.body.appendChild(div);
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
